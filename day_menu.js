9b0856000142bf4f8cc4fc1bdd9856de68c67a15
Bruno Azevedo <bruno.azevedo@toogas.com>
2021-05-22 12:50:34 +0100

    Atualizaçoes no layout e no cardapio da semana

--- day_menu.js
+++ day_menu.js
@@ -1,38 +1,48 @@
 const day_menu = [
     {
         "idProdutos": "1",
-        "foto": "./img/example1.png",
+        "foto": "./img/pratos/1.png",
         "preco": "4,50",
-        "descricao": "Frango à parmegiana, brócolos, cenoura e quinoa<br>(arroz opcional)"
+        "descricao": "Filé de frango de forno, quinoa, batata doce e brócolos"
     },
     {
         "idProdutos": "2",
-        "foto": "./img/example1.png",
+        "foto": "./img/pratos/2.png",
         "preco": "4,50",
-        "descricao": "Frango à parmegiana, brócolos, cenoura e quinoa<br>(arroz opcional)"
+        "descricao": "Bifana, lentilhas, brócolos, cenoura e couve-flor"
     },
     {
         "idProdutos": "3",
-        "foto": "./img/example1.png",
+        "foto": "./img/pratos/3.png",
         "preco": "4,50",
-        "descricao": "Frango à parmegiana, brócolos, cenoura e quinoa<br>(arroz opcional)"
+        "descricao": "Rolo de carne, arroz integral, feijão preto, chuchu e cenoura"
     },
     {
         "idProdutos": "4",
-        "foto": "./img/example1.png",
-        "preco": "4,50",
-        "descricao": "Frango à parmegiana, brócolos, cenoura e quinoa<br>(arroz opcional)"
+        "foto": "./img/pratos/4.png",
+        "preco": "5,00",
+        "descricao": "Salmão, arroz integral, brócolos e couve-flor"
     },
     {
         "idProdutos": "5",
-        "foto": "./img/example1.png",
+        "foto": "./img/pratos/5.png",
         "preco": "4,50",
-        "descricao": "Frango à parmegiana, brócolos, cenoura e quinoa<br>(arroz opcional)"
+        "descricao": "Filé de pescada, arroz integral, curgete, beterraba e chuchu"
     },
     {
         "idProdutos": "6",
-        "foto": "./img/example1.png",
-        "preco": "5,00",
-        "descricao": "Frango à parmegiana, brócolos, cenoura e quinoa<br>(arroz opcional)"
+        "foto": "./img/pratos/6.png",
+        "preco": "4,50",
+        "descricao": "Carne refogada, purê de batata doce, curgete, cenoura e couve-flor"
     },
 ];
+
+/*
+
+    {
+        "idProdutos": "2-1",
+        "foto": "./img/example1.png",
+        "preco": "4,50",
+        "descricao": "Frango de forno com queijo e orégano, arroz integral, beterraba, cenoura e chuchu"
+    },
+    */
